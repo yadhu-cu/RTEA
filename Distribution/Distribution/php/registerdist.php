6ddfba28efe70135c279cbad661bfcefa2d3315d
Sreelekhatk <54243520+Sreelekhatk@users.noreply.github.com>
2020-02-22 16:00:39 +0530

    include file added

--- Distribution/Distribution/php/registerdist.php
+++ Distribution/Distribution/php/registerdist.php
@@ -1,5 +1,5 @@
 <?php
-include('../connection.php');
+include('../../../Backend/PHP/Connection.php');
 $distuname= $_POST['distuname'];
 $distPlace=$_POST['distPlace'];
 $phoneNumber=$_POST['phoneNumber'];
@@ -7,5 +7,5 @@ $password=$_POST['password'];
 
 $query="INSERT INTO `distribution_info`(`Place`, `name`, `phoneNumber`, `Password`) VALUES ('$distPlace', '$distuname', '$phoneNumber', '$password')";
 $result= mysqli_query($con,$query);
-// header('Location:../index.html');
+header('Location:../index.html');
 ?>
