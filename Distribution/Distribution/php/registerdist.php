92c9a8ab71b66ba11697e8a2b3de8ec663240084
Sreelekhatk <54243520+Sreelekhatk@users.noreply.github.com>
2020-02-22 15:42:01 +0530

    Update registerdist.php

--- Distribution/Distribution/php/registerdist.php
+++ Distribution/Distribution/php/registerdist.php
@@ -4,7 +4,8 @@ $distuname= $_POST['distuname'];
 $distPlace=$_POST['distPlace'];
 $phoneNumber=$_POST['phoneNumber'];
 $password=$_POST['password'];
+
 $query="INSERT INTO `distribution_info`(`Place`, `name`, `phoneNumber`, `Password`) VALUES ('$distPlace', '$distuname', '$phoneNumber', '$password')";
-$result= mysqli_query($link,$query);
-header('Location:../index.html');
+$result= mysqli_query($con,$query);
+// header('Location:../index.html');
 ?>
