916e0a4ff00c0e4a2efa1a89804ae2ddc656d2d7
Sreelekhatk <54243520+Sreelekhatk@users.noreply.github.com>
2020-03-02 12:14:34 +0530

    marketing register update

--- BackEnd/Php/User/RegisterInvent.php
+++ BackEnd/Php/User/RegisterInvent.php
@@ -1,13 +1,14 @@
 <?php
 include('../connection.php');
 
-$CompanyName = $_POST['userName'];
+$CompanyName = $_POST['companyName'];
+$CompanyAddr = $_POST['companyAddress'];
 $password = $_POST['password'];
 $phoneNumber = $_POST['phoneNumber'];
 //$email_ID = $_POST['email_ID'];
 
 
-$query="INSERT INTO `inventory`( `phoneNumber`, `companyName`, `password`) VALUES  ('$phoneNumber','$CompanyName','$password')";
+$query="INSERT INTO `inventory`(`phoneNumber`, `companyName`, `companyAddress`, `password`, `location`) VALUES('$phoneNumber','$CompanyName','$password')";
 $result=mysqli_query($con,$query); 
 
 //header('Location:../profile.php');
